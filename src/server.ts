16d890baa58bbe8d566166723d63137347c07961
leuzery <valentyn.hvozdenko@htlstp.at>
2025-07-14 15:06:55 +0200

    add basic socket structure

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,26 @@
 import app from './app'
 import connectDB from './config/db'
 import {config} from './config/config'
+import http from 'http'
+import {Server} from 'socket.io'
+import { spacesHandler } from 'modules/spaces/spaces.socket'
+
+
 
 const startServer = async () => {
     try {
         await connectDB()
 
-        app.listen(config.PORT, () => {
+        const httpServer = http.createServer(app);
+        const io = new Server(httpServer, {
+            cors: { origin: '*' },
+        });
+
+        io.on("connection", (socket) => {
+            spacesHandler(io, socket)
+        })
+
+        httpServer.listen(config.PORT, () => {
             console.log(`Server runs on http://localhost:${config.PORT}`)
         })
     } catch (error: any) {

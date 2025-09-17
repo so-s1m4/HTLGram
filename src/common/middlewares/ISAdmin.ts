18f9c0f688746808b21171398a015e400508737e
so-s1m4 <maks.rvachov.at@gmail.com>
2025-09-17 14:14:22 +0200

    ISAdmin middleware

---
+++ src/common/middlewares/ISAdmin.ts
@@ -0,0 +1,29 @@
+import { Request, Response, NextFunction } from 'express'
+import { ErrorWithStatus } from './errorHandlerMiddleware'
+import usersService from '../../modules/users/users.service'
+
+const JWTMiddleware = (req: Request, res: Response, next: NextFunction) => {
+	try {
+		const data = res.locals.user
+		if (!data || typeof data === 'string') {
+			throw new ErrorWithStatus(401, 'Invalid token')
+		}
+		usersService
+			.getMyData(data.userId)
+			.then(userData => {
+				if (userData.role !== 'admin') {
+					throw new ErrorWithStatus(403, 'Forbidden')
+				}
+				res.locals.userData = userData
+				next()
+			})
+			.catch(err => {
+				next(new ErrorWithStatus(401, err.message))
+			})
+	} catch (e: unknown) {
+		const err = e instanceof Error ? e : new Error('Unexpected error')
+		next(new ErrorWithStatus(401, err.message))
+	}
+}
+
+export default JWTMiddleware
--- src/modules/admin/admin.routes.ts
+++ src/modules/admin/admin.routes.ts
@@ -2,44 +2,45 @@ import express from 'express'
 import adminController from './admin.controller'
 import { ErrorWrapper } from '../../common/utils/utils.wrappers'
 import JWTMiddleware from '../../common/middlewares/JWTMiddleware'
+import ISAdmin from '../../common/middlewares/ISAdmin'
 import { upload } from '../../common/multer/multer.photo'
 
 const router = express.Router()
 
 // Users
-router.get('/users', JWTMiddleware, ErrorWrapper(adminController.users.getUsers))
-router.post('/users', JWTMiddleware, ErrorWrapper(adminController.users.createUser))
+router.get('/users', JWTMiddleware, ISAdmin, ErrorWrapper(adminController.users.getUsers))
+router.post('/users', JWTMiddleware, ISAdmin, ErrorWrapper(adminController.users.createUser))
 
-router.get('/users/:userId', JWTMiddleware, ErrorWrapper(adminController.users.getUserById))
-router.patch('/users/:userId', JWTMiddleware, ErrorWrapper(adminController.users.updateUserById))
-router.delete('/users/:userId', JWTMiddleware, ErrorWrapper(adminController.users.deleteUserById))
+router.get('/users/:userId', JWTMiddleware, ISAdmin, ErrorWrapper(adminController.users.getUserById))
+router.patch('/users/:userId', JWTMiddleware, ISAdmin, ErrorWrapper(adminController.users.updateUserById))
+router.delete('/users/:userId', JWTMiddleware, ISAdmin, ErrorWrapper(adminController.users.deleteUserById))
 
-router.get('/users/:userId/gifts', JWTMiddleware, ErrorWrapper(adminController.users.getUserGifts))
-router.get('/users/:userId/gifts/:giftId', JWTMiddleware, ErrorWrapper(adminController.users.getUserGiftById))
-router.patch('/users/:userId/gifts/:giftId', JWTMiddleware, ErrorWrapper(adminController.users.updateUserGiftById))
-router.delete('/users/:userId/gifts/:giftId', JWTMiddleware, ErrorWrapper(adminController.users.deleteUserGiftById))
+router.get('/users/:userId/gifts', JWTMiddleware, ISAdmin, ErrorWrapper(adminController.users.getUserGifts))
+router.get('/users/:userId/gifts/:giftId', JWTMiddleware, ISAdmin, ErrorWrapper(adminController.users.getUserGiftById))
+router.patch('/users/:userId/gifts/:giftId', JWTMiddleware, ISAdmin, ErrorWrapper(adminController.users.updateUserGiftById))
+router.delete('/users/:userId/gifts/:giftId', JWTMiddleware, ISAdmin, ErrorWrapper(adminController.users.deleteUserGiftById))
 
 // Messages
-router.get('/messages', JWTMiddleware, ErrorWrapper(adminController.messages.getMessages))
-router.post('/messages', JWTMiddleware, ErrorWrapper(adminController.messages.createMessage))
+router.get('/messages', JWTMiddleware, ISAdmin, ErrorWrapper(adminController.messages.getMessages))
+router.post('/messages', JWTMiddleware, ISAdmin, ErrorWrapper(adminController.messages.createMessage))
 
-router.get('/messages/:messageId', JWTMiddleware, ErrorWrapper(adminController.messages.getMessageById))
-router.patch('/messages/:messageId', JWTMiddleware, ErrorWrapper(adminController.messages.updateMessageById))
-router.delete('/messages/:messageId', JWTMiddleware, ErrorWrapper(adminController.messages.deleteMessageById))
+router.get('/messages/:messageId', JWTMiddleware, ISAdmin, ErrorWrapper(adminController.messages.getMessageById))
+router.patch('/messages/:messageId', JWTMiddleware, ISAdmin, ErrorWrapper(adminController.messages.updateMessageById))
+router.delete('/messages/:messageId', JWTMiddleware, ISAdmin, ErrorWrapper(adminController.messages.deleteMessageById))
 
 // Gifts
-router.get('/gifts', JWTMiddleware, ErrorWrapper(adminController.gifts.getGifts))
-router.post('/gifts', JWTMiddleware, ErrorWrapper(adminController.gifts.createGift))
-router.get('/gifts/:giftId', JWTMiddleware, ErrorWrapper(adminController.gifts.getGiftById))
-router.patch('/gifts/:giftId', JWTMiddleware, ErrorWrapper(adminController.gifts.updateGiftById))
-router.delete('/gifts/:giftId', JWTMiddleware, ErrorWrapper(adminController.gifts.deleteGiftById))
+router.get('/gifts', JWTMiddleware, ISAdmin, ErrorWrapper(adminController.gifts.getGifts))
+router.post('/gifts', JWTMiddleware, ISAdmin, ErrorWrapper(adminController.gifts.createGift))
+router.get('/gifts/:giftId', JWTMiddleware, ISAdmin, ErrorWrapper(adminController.gifts.getGiftById))
+router.patch('/gifts/:giftId', JWTMiddleware, ISAdmin, ErrorWrapper(adminController.gifts.updateGiftById))
+router.delete('/gifts/:giftId', JWTMiddleware, ISAdmin, ErrorWrapper(adminController.gifts.deleteGiftById))
 
 // Emojis
-router.get('/emojis', JWTMiddleware, ErrorWrapper(adminController.emojis.getEmojis))
-router.post('/emojis', JWTMiddleware, ErrorWrapper(adminController.emojis.createEmoji))
-router.get('/emojis/:emojiId', JWTMiddleware, ErrorWrapper(adminController.emojis.getEmojiById))
-router.patch('/emojis/:emojiId', JWTMiddleware, ErrorWrapper(adminController.emojis.updateEmojiById))
-router.delete('/emojis/:emojiId', JWTMiddleware, ErrorWrapper(adminController.emojis.deleteEmojiById))
+router.get('/emojis', JWTMiddleware, ISAdmin, ErrorWrapper(adminController.emojis.getEmojis))
+router.post('/emojis', JWTMiddleware, ISAdmin, ErrorWrapper(adminController.emojis.createEmoji))
+router.get('/emojis/:emojiId', JWTMiddleware, ISAdmin, ErrorWrapper(adminController.emojis.getEmojiById))
+router.patch('/emojis/:emojiId', JWTMiddleware, ISAdmin, ErrorWrapper(adminController.emojis.updateEmojiById))
+router.delete('/emojis/:emojiId', JWTMiddleware, ISAdmin, ErrorWrapper(adminController.emojis.deleteEmojiById))
 
 
 export default router
